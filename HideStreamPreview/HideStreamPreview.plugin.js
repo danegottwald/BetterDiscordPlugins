0e2a392ebc73aedc9ab28c0e04ca5ecbd3e37993
danegottwald <danegottwald@gmail.com>
2021-08-11 11:48:40 -0700

    settings panel dynamic construction

--- HideStreamPreview/HideStreamPreview.plugin.js
+++ HideStreamPreview/HideStreamPreview.plugin.js
@@ -1,13 +1,13 @@
 /**
-* @name HideStreamPreview
-* @author blurrpy
-* @description Hide your own stream preview in multistream calls.
-* @version 0.0.2
-* @authorLink https://github.com/danegottwald
-* @website https://github.com/danegottwald
-* @donate https://www.paypal.com/paypalme/danegottwald
-* @source https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js
-*/
+ * @name HideStreamPreview
+ * @author blurrpy
+ * @description Hide your own stream preview in multistream calls.
+ * @version 0.0.3
+ * @authorLink https://github.com/danegottwald
+ * @website https://github.com/danegottwald
+ * @donate https://www.paypal.com/paypalme/danegottwald
+ * @source https://raw.githubusercontent.com/danegottwald/BetterDiscordPlugins/main/HideStreamPreview/HideStreamPreview.plugin.js
+ */
 
 const fs = require("fs");
 const path = require("path");
@@ -21,22 +21,23 @@ const config = {
             "discord_id": "154401402263699457",
             "github_username": "danegottwald"
         }],
-        "version": "0.0.2",
+        "version": "0.0.3",
         "description": "Hide your own stream preview when screen sharing with multiple users",
         "github": "",
         "github_raw": ""
     },
     "changelog": [
-        {"title": "New Stuff", "items": ["Added more settings", "Added changelog"]},
-        {"title": "Bugs Squashed", "type": "fixed", "items": ["React errors on reload"]},
-        {"title": "Improvements", "type": "improved", "items": ["Improvements to the base plugin"]},
-        {"title": "On-going", "type": "progress", "items": ["More modals and popouts being added", "More classes and modules being added"]}
+        //{"title": "New Stuff", "items": ["Added more settings", "Added changelog"]},
     ],
     "main": "HideStreamPreview.js"
 };
 
 var settings = {
-    "showWhenLowStreams": true
+    "showWhenLowStreams": {
+        "title": "Show Own Stream At Low Stream Count (1-2 streams)",
+        "description": "Displays stream preview when there are less than 3 active streams",
+        "value": true
+    }
 }
 
 module.exports = !global.ZeresPluginLibrary ? class {
@@ -51,9 +52,9 @@ module.exports = !global.ZeresPluginLibrary ? class {
             cancelText: "Cancel",
             onConfirm: () => {
                 request.get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", (error, response, body) => {
-                    if (error)
+                    if (error) {
                         return electron.shell.openExternal("https://github.com/rauenzi/BDPluginLibrary");
-
+                    }
                     fs.writeFileSync(path.join(BdApi.Plugins.folder, "HideStreamPreview.plugin.js"), body);
                 });
             }
@@ -75,32 +76,38 @@ module.exports = !global.ZeresPluginLibrary ? class {
         }
 
         onStart() {
-            ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "temp-link");
+            // Check for Plugin Updates
+            ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/danegottwald/BetterDiscordPlugins/main/HideStreamPreview/HideStreamPreview.plugin.js");
 
             Patcher.before(Logger, "log", (t, a) => {
                 a[0] = "Patched Message: " + a[0];
             });
-            settings = PluginUtilities.loadSettings("HideStreamPreview", {});
+            
+            // Load Settings from Config on Startup
+            Object.entries(PluginUtilities.loadData("HideStreamPreview", "settings", {})).forEach(([setting, value]) => {
+                settings[setting]["value"] = value
+            });
         }
 
         onStop() {
             Patcher.unpatchAll();
-            PluginUtilities.saveSettings("HideStreamPreview", settings);
         }
 
         getSettingsPanel () {
-            settings = PluginUtilities.loadSettings("HideStreamPreview", {});
-
-            let template = new Library.Settings.Switch("Show Own Stream At Low Stream Count (1-2 streams)", 
-                "Displays stream preview when there are less than 3 active streams", 
-                settings["showWhenLowStreams"], 
-                (val) => {
-                    settings["showWhenLowStreams"] = val;
-                    PluginUtilities.saveSettings("HideStreamPreview", settings);
-                }
-            );
-
-            return template.getElement();
+            var panel = new Library.Settings.SettingPanel();
+            Object.entries(settings).forEach(([setting, content]) => {
+                panel.append(
+                    new Library.Settings.Switch(
+                        content["title"], content["description"], content["value"], 
+                        (val) => {
+                            settings[setting]["value"] = val;
+                            PluginUtilities.saveSettings("HideStreamPreview", { [setting]: val });
+                        }
+                    )
+                );
+            });
+                
+            return panel.getElement();
         }
 
         // Hide stream preview when the wrapper for the video tiles is targeted
@@ -112,7 +119,7 @@ module.exports = !global.ZeresPluginLibrary ? class {
 
         _hideStreamPreview() {
             // Only hide stream preview if there are three or more streams OR if setting is false
-            if (!settings["showWhenLowStreams"] || StreamStore.getAllActiveStreams().length >= 3) {
+            if (!settings["showWhenLowStreams"]["value"] || StreamStore.getAllActiveStreams().length >= 3) {
                 let element = Array.from(document.getElementsByTagName('span')).find(span =>
                     ((span.textContent == DiscordAPI.currentUser.discordObject.username || 
                         span.textContent == DiscordAPI.currentGuild.members[0].nickname) &&
@@ -121,11 +128,10 @@ module.exports = !global.ZeresPluginLibrary ? class {
                 if (element) {
                     while (element.parentElement && !element.parentElement.classList.contains("tile-kezkfV")) {
                         element = element.parentElement;
-                        if (element.parentElement == null) {
-                            return;
-                        }
                     }
-                    element.parentElement.style.display = "none";
+                    if (element.parentElement != null) {
+                        element.parentElement.style.display = "none";
+                    }
                 }
             }
         }
